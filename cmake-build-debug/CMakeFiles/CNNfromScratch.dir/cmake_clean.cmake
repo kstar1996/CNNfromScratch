86420aa8514282a0c307e5ab61040aada52a835b
Eujin Kim <eujin@uxf.ai>
2021-02-24 10:50:58 +0900

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/CNNfromScratch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CNNfromScratch.dir/main.cpp.o"
+  "CNNfromScratch"
+  "CNNfromScratch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CNNfromScratch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CNNfromScratch.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CNNfromScratch.
+# This may be replaced when dependencies are built.
