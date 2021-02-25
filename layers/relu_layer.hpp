a8a3f728adfc76023c1304ceb4acf4867ec1f692
Eujin Kim <eujin@uxf.ai>
2021-02-25 15:21:22 +0900

    Relu Layer

---
+++ layers/relu_layer.hpp
@@ -0,0 +1,28 @@
+//
+// Created by eujin on 2/25/21.
+//
+
+#ifndef CNNFROMSCRATCH_RELU_LAYER_HPP
+#define CNNFROMSCRATCH_RELU_LAYER_HPP
+
+#endif //CNNFROMSCRATCH_RELU_LAYER_HPP
+
+#include <iostream>
+#include <armadillo>
+
+class ReluLayer{
+public:
+    //constructor implementation
+    ReluLayer(size_t inputHeight,
+              size_t inputWidth,
+              size_t inputDepth) :
+              inputHeight(inputHeight),
+              inputWidth(inputWidth),
+              inputDepth(inputDepth){//nothing to do
+    }
+    void Forward(amr)
+
+
+
+
+};
