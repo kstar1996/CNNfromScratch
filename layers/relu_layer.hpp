3608a2e4b85a528dca12c77da363952e52d852f4
Eujin Kim <eujin@uxf.ai>
2021-02-26 14:14:24 +0900

    Relu Layer add on

--- layers/relu_layer.hpp
+++ layers/relu_layer.hpp
@@ -20,9 +20,34 @@ public:
               inputWidth(inputWidth),
               inputDepth(inputDepth){//nothing to do
     }
-    void Forward(amr)
+    void Forward(arma::cube& input, arma::cube& output)
+    {
+        output = arma::zeros(arma::size(input));
+        output = arma::max(input, output);
+        this->input = input;
+        this->output = output;
+    }
 
+    void Backward(arma::cube upstreamGradient)
+    {
+        gradientWrtInput = input;
+        gradientWrtInput.transform( [](double val) { return val > 0? 1 : 0; } );
+        gradientWrtInput = gradientWrtInput % upstreamGradient;
+    }
+    arma::cube getGradientWrtInput() { return gradientWrtInput; }
 
+private:
+    size_t inputHeight;
+    size_t inputWidth;
+    size_t inputDepth;
+
+    arma::cube input;
+    arma::cube output;
+
+    arma::cube gradientWrtInput;
+};
+
+#endif
 
 
 };
